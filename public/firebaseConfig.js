092b79cf21ca2fc5fce25fe1b2d6f866c923b850
Craig.Williams <craigjmwilliams@gmail.com>
2025-04-14 12:32:11 -0400

    Made changes

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBnyTSJNB9SOsx1-RPySp7OOzEOK06rM0c",
-    authDomain: "info1601-booksapp.firebaseapp.com",
-    projectId: "info1601-booksapp",
-    storageBucket: "info1601-booksapp.firebasestorage.app",
-    messagingSenderId: "568459881258",
-    appId: "1:568459881258:web:a37d518df9686e77120c53"
-  };
-
+  apiKey: "AIzaSyBpWSipKSYa_FyblRMcw-b5gg0OZ5tRASY",
+  authDomain: "info1601l9.firebaseapp.com",
+  projectId: "info1601l9",
+  storageBucket: "info1601l9.firebasestorage.app",
+  messagingSenderId: "674479609490",
+  appId: "1:674479609490:web:e5bf70a305e9fe8d00f84b",
+  measurementId: "G-GZNRGD28NJ"
+};
 
   export default firebaseConfig;
